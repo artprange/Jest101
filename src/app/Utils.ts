bad5119b1ee8d2951a120a467a19daac207d3fc2
artprange <arthur.prange@gmail.com>
2024-07-23 09:55:08 -0300

    defining tests

--- src/app/Utils.ts
+++ src/app/Utils.ts
@@ -1,3 +1,22 @@
 export function toUpperCase(arg: string){
-    return arg.toLocaleUpperCase()
+    return arg.toUpperCase()
+}
+
+
+export type StringInfo = {
+    lowerCase: string
+    upperCase: string
+    characters: string[]
+    length: number
+    extraInfo: Object | undefined 
+}
+
+export function getStringInfo(arg: string): StringInfo{
+    return{
+        lowerCase: arg.toLowerCase(),
+        upperCase: arg.toUpperCase(),
+        characters: Array.from(arg),
+        length: arg.length,
+        extraInfo: {}
+    }
 }
--- src/test/Utils.test.ts
+++ src/test/Utils.test.ts
@@ -1,8 +1,73 @@
-import { toUpperCase } from "../app/Utils"
+import { getStringInfo, toUpperCase } from "../app/Utils"
 
 describe('Utils test suite', ()=>{
-    test('should return uppercase', ()=>{
-        const result = toUpperCase('abc')
-        expect(result).toBe("ABC")
+    it('should return uppercase of a valid string', ()=>{
+        //ARRANGE:
+        const sut  = toUpperCase
+        const expected = 'ABC'
+
+        //ACT:
+        const actual = toUpperCase('abc')  
+        
+        //ASSERT:
+        expect(actual).toBe(expected)     
     })
+
+        //jeito certo, mas não é  a melhor prática
+
+       /* it('should return info for valid string', ()=>{
+        //arranging
+        const actual = getStringInfo('My-string')
+        
+        expect(actual.lowerCase).toBe('my-string')
+        expect(actual.extraInfo).toEqual({})
+
+        //expect(actual.characters.length).toBe(9)
+        expect(actual.characters).toHaveLength(9)
+        expect(actual.characters).toEqual(['M', 'y', '-', 's','t','r','i','n', 'g'])
+        expect(actual.characters).toContain<string>('M')
+        expect(actual.characters).toEqual(expect.arrayContaining((['t','r','i','n', 'g','M', 'y', '-', 's' ])))
+
+        expect(actual.extraInfo).not.toBe(undefined) 
+        //outra maneira de saber se é o objeto é VÁLIDO, é usar:
+        expect(actual.extraInfo).toBeTruthy()
+
+
+    })*/
+
+    describe.only('getStringInfo for arg My-string should', ()=>{
+        test('return right length', ()=>{
+            const actual = getStringInfo('My-string')
+            expect(actual.characters).toHaveLength(9)
+        })
+
+        test('return right lower case', ()=>{
+            const actual = getStringInfo('My-string')
+            expect(actual.lowerCase).toBe('my-string')
+        })
+
+        test('return right upper case', ()=>{
+            const actual = getStringInfo('My-string')
+            expect(actual.upperCase).toBe('MY-STRING')
+        })
+
+        test('return  right characters', ()=>{
+            const actual = getStringInfo('My-string')
+            expect(actual.characters).toEqual(['M', 'y', '-', 's','t','r','i','n', 'g'])
+            expect(actual.characters).toContain<string>('M')
+            expect(actual.characters).toEqual(expect.arrayContaining((['t','r','i','n', 'g','M', 'y', '-', 's' ])))
+        })
+
+        test('return defined extra info', ()=>{
+            const actual = getStringInfo('My-string')
+            expect(actual.extraInfo).toBeTruthy()
+        })
+
+        test('return defined extra info', ()=>{
+            const actual = getStringInfo('My-string')
+            expect(actual.extraInfo).toEqual({})
+        })
+    })
+
+
 })
